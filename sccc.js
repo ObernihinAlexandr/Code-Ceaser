91ab90a44cdad2cda2326d055b5148efbde96f0b
ObernihinAlexandr <36127394+ObernihinAlexandr@users.noreply.github.com>
2018-02-04 13:01:11 +0200

    Add files via upload

---
+++ sccc.js
@@ -0,0 +1,12 @@
+function caesar_code(a, d) { 
+var c = document.getElementById("1_text1"), 
+g = document.getElementById("2_text2"), 
+b = "ABCDEFGHIJKLMNOPQRSTUVWXYZ", 
+e = "", 
+d = d % b.length || 0, 
+c = c.value.toUpperCase(), 
+f = b.substr(d) + b.substr(0, d); 
+1 == a && (a = b, b = f, f = a); 
+for (i = 0; i < c.length; i++) a = b.lastIndexOf(c.charAt(i)), a = f.charAt(a) || c.charAt(i), e += a; 
+g.value = e 
+}; 
